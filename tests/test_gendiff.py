40a5b4aff9b103588c8c546206a91084ecf90277
nickolay_kuzmin <nickolay.kuzmin2001@gmail.com>
2022-12-01 16:10:02 +0400

    Create first test for gendiff.

---
+++ tests/test_gendiff.py
@@ -0,0 +1,22 @@
+import json
+import pytest
+from gendiff.gendiff import generate_diff
+
+
+@pytest.fixture()
+def expected():
+    with open('./fixtures/expected_diff', mode='r') as file:
+        return file.readlines()
+
+
+@pytest.fixture()
+def json_files():
+    path1 = './fixtures/file1.json'
+    path2 = './fixtures/file2.json'
+    json_file1 = json.load(open(file=path1, mode='r'))
+    json_file2 = json.load(open(file=path2, mode='r'))
+    return json_file1, json_file2
+
+
+def test_gendiff(json_files, expected):
+    assert generate_diff(*json_files) == expected
