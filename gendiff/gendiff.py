30d390b78d3869239104505ed101283d1d1c07b5
nickolay_kuzmin <nickolay.kuzmin2001@gmail.com>
2022-12-08 15:10:16 +0400

    Refactor gendiff.

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,17 +1,11 @@
 def generate_diff(file1: dict, file2: dict):
-    keys1 = list(file1.keys())
-    keys2 = list(file2.keys())
-    keys = sorted(set(keys1 + keys2))
+    keys = sorted({*file2.keys(), *file1.keys()})
     diff_str = ""
     for key in keys:
         value1 = file1.get(key)
         value2 = file2.get(key)
-        if value1 in [True, False]:
-            value1 = str(value1).lower()
-        if value2 in [True, False]:
-            value2 = str(value2).lower()
         if value1 == value2:
-            diff_str += f'    {key}: {value1}\n'
+            diff_str += f'    {key}: {value2}\n'
         elif value1 is None:
             diff_str += f'  + {key}: {value2}\n'
         elif value2 is None:
@@ -19,4 +13,6 @@ def generate_diff(file1: dict, file2: dict):
         else:
             diff_str += f'  - {key}: {value1}\n'
             diff_str += f'  + {key}: {value2}\n'
+    diff_str = diff_str.replace('True', 'true')
+    diff_str = diff_str.replace('False', 'false')
     return '{\n' + diff_str + '}'
