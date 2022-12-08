967a7db86e9b4649d0b21ef96ce75314bf9dd8ea
nickolay_kuzmin <nickolay.kuzmin2001@gmail.com>
2022-12-08 16:44:10 +0400

    Add deep fixtures. Create parametrize tests.

--- tests/test_gendiff_jsons.py
+++ tests/test_gendiff_jsons.py
@@ -3,20 +3,22 @@ from gendiff.gendiff import generate_diff
 from gendiff.utils import load_jsons
 
 
-@pytest.fixture()
-def expected():
-    with open('tests/fixtures/jsons/expected_diff', mode='r') as file:
+@pytest.fixture
+def json_expected(dir_):
+    path = f'tests/fixtures/jsons/{dir_}/expected_diff'
+    with open(file=path, mode='r') as file:
         expected = file.readlines()
         expected = str.join('', expected)
         return expected
 
 
-@pytest.fixture()
-def json_files():
-    path1 = 'tests/fixtures/jsons/file1.json'
-    path2 = 'tests/fixtures/jsons/file2.json'
+@pytest.fixture
+def json_files(dir_):
+    path1 = f'tests/fixtures/jsons/{dir_}/file1.json'
+    path2 = f'tests/fixtures/jsons/{dir_}/file2.json'
     return load_jsons(path1, path2)
 
 
-def test_gendiff(json_files, expected):
-    assert generate_diff(*json_files) == expected
+@pytest.mark.parametrize(argvalues=['simple_jsons', 'deep_jsons'], argnames='dir_')
+def test_gendiff(json_files, json_expected):
+    assert generate_diff(*json_files) == json_expected
