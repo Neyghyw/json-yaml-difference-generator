4ba9cfdcf9caa5c9b44eff3af0636553d039bd91
nickolay_kuzmin <nickolay.kuzmin2001@gmail.com>
2022-12-08 16:44:10 +0400

    Add deep fixtures. Create parametrize tests.

--- tests/test_gendiff_yaml.py
+++ tests/test_gendiff_yaml.py
@@ -3,20 +3,23 @@ from gendiff.gendiff import generate_diff
 from gendiff.utils import load_yamls
 
 
-@pytest.fixture()
-def expected():
-    with open('tests/fixtures/yamls/expected_diff', mode='r') as file:
+@pytest.fixture
+def yaml_expected(dir_):
+    path = f'tests/fixtures/yamls/{dir_}/expected_diff'
+    with open(file=path, mode='r') as file:
         expected = file.readlines()
         expected = str.join('', expected)
         return expected
 
 
-@pytest.fixture()
-def yaml_files():
-    path1 = 'tests/fixtures/yamls/file1.yaml'
-    path2 = 'tests/fixtures/yamls/file2.yaml'
+@pytest.fixture
+def yaml_files(dir_):
+    path1 = f'tests/fixtures/yamls/{dir_}/file1.yaml'
+    path2 = f'tests/fixtures/yamls/{dir_}/file2.yaml'
     return load_yamls(path1, path2)
 
 
-def test_gendiff(yaml_files, expected):
-    assert generate_diff(*yaml_files) == expected
+@pytest.mark.parametrize(argvalues=['simple_yamls', 'deep_yamls'], argnames='dir_')
+def test_gendiff(yaml_files, yaml_expected):
+    assert generate_diff(*yaml_files) == yaml_expected
+
