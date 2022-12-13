b719489750da513c3f011571eed17b1d0462cf89
nickolay_kuzmin <nickolay.kuzmin2001@gmail.com>
2022-12-13 16:54:13 +0400

    Work version 2

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,38 +1,43 @@
 from .formatters.stylish import make_stylish
-from .utils.from_format.utils import replace_unsuitable
+from .utils.from_format.utils import format_wrong_words
+from .utils.from_format.utils import modify_untapped_keys
 
 
-def generate_diff(file1: dict, file2: dict):
-    diff_dict = dict()
-    fields = sorted({*file2.keys(), *file1.keys()})
+def make_keymap(table, dict1, dict2):
+    keys = sorted({*dict1.keys(), *dict2.keys()})
+    for key in keys:
+        items = []
+        if key in dict1.keys():
+            items.append(dict1[key])
+        if key in dict2.keys():
+            items.append(dict2[key])
+        table[key] = items
 
-    def walk(key):
-        value1 = file1.get(key, 'netu')
-        value2 = file2.get(key, 'netu')
-        if isinstance(value1, dict) and isinstance(value2, dict):
-            diff_dict[f'   {key}'] = generate_diff(value1, value2)
-        elif isinstance(value1, dict):
-            diff_dict[f' - {key}'] = generate_diff(value1, value1)
-            if value2 != 'netu':
-                diff_dict[f' + {key}'] = value2
-        elif isinstance(value2, dict):
-            if value1 != 'netu':
-                diff_dict[f' - {key}'] = value1
-            diff_dict[f' + {key}'] = generate_diff(value2, value2)
-        elif value1 == value2:
-            diff_dict[f'   {key}'] = value1
-        elif value1 == 'netu':
-            diff_dict[f' + {key}'] = value2
-        elif value2 == 'netu':
-            diff_dict[f' - {key}'] = value1
+
+def generate_diff(dict1, dict2):
+    diff = dict()
+    key_map = dict()
+    make_keymap(key_map, dict1, dict2)
+
+    for key, values in key_map.items():
+        if len(values) == 1:
+            mark = '-' if key in dict1.keys() else '+'
+            value = values[0]
+            diff[f' {mark} {key}'] = value
         else:
-            diff_dict[f' - {key}'] = value1
-            diff_dict[f' + {key}'] = value2
-    for field in fields:
-        walk(field)
-    return diff_dict
+            first, second = values
+            if isinstance(first, dict) and isinstance(second, dict):
+                diff[f'   {key}'] = generate_diff(first, second)
+            elif first == second:
+                diff[f'   {key}'] = first
+            else:
+                diff[f' - {key}'] = first
+                diff[f' + {key}'] = second
+    return diff
 
 
 def stringify(diff: dict):
-    result = make_stylish(diff)
-    return replace_unsuitable(result)
+    diff = modify_untapped_keys(diff)
+    stringifyed = make_stylish(diff)
+    stringifyed = format_wrong_words(stringifyed)
+    return stringifyed
