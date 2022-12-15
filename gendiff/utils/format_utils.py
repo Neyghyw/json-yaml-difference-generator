b1c392d2df4162a471ea0f70881c26ba5795f459
nickolay_kuzmin <nickolay.kuzmin2001@gmail.com>
2022-12-15 22:33:00 +0400

    Refactor for hexlet-tests

--- gendiff/utils/format_utils.py
+++ gendiff/utils/format_utils.py
@@ -2,7 +2,7 @@ def format_wrong_words(value: str):
     value = value.replace('True', 'true')
     value = value.replace('False', 'false')
     value = value.replace('None', 'null')
-    value = value.replace(': \n', ':\n')
+    # value = value.replace(': \n', ':\n')
     return value
 
 
